b5241a54d2f8c7e2ef27ca44fe2a07a56bc7b79d
kyrex32 <138849020+kyrex32@users.noreply.github.com>
2025-02-08 22:07:06 +0300

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Helper • v1.2"];
+const statusMessages = ["www.roblox.com, Made by Rincaste"];
 const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
